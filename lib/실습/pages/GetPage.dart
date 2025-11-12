2937fcfc8f93ef9675c7b849adafafc29ac6ed79
rutaku12rutaku12 <chookyuho@gmail.com>
2025-11-13 00:10:19 +0900

    왜 조회가?

--- lib/실습/pages/GetPage.dart
+++ lib/실습/pages/GetPage.dart
@@ -10,7 +10,7 @@ class GetPageState extends State<GetPage>{
   void getTodoList() async{
     try{
       final dio = Dio();
-      final response = await dio.get("http://10.0.2.2:8080/api/goods/list");
+      final response = await dio.get("http://10.0.2.2:8080/api/todo");
       final data = response.data;
       print(data);
       setState(() { todoList = data; } );
